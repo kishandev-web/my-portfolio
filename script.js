c4e439fc8181ee9993bbcfade2167f5e3335185a
Kishan Gupta <kishanguptaayodhya123@gmail.com>
2025-01-28 17:11:13 +0530

    Add Javascript flie and some changes in html css

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Elements
+
+
+const aboutSection = document.querySelector('#click-about');
+const resumeSection = document.querySelector('#click-resume');
+const addDark = document.querySelector('.dark');
+const cvDownload = document.querySelector('.cv');
+const contact = document.querySelector('.contact');
